f6a2d154ba2344bc247cce7606f3d82226cf7c38
Advaith Chaitanya <advaithchaitanya@gmail.com>
2025-07-29 16:18:00 +0530

    answers are in assessment/txt file

--- Assessment/Answers.java
+++ Assessment/Answers.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Answers {
     public int add(int a,int b){
         return  a+b;
@@ -13,7 +14,6 @@ public class Answers {
     }
     public static void main(String[] args) {
         Answers ans = new Answers();
-
         System.out.println("Print adding: " + ans.add(7, 63));
         System.out.println("Print even check: " + ans.iseven(ans.add(7, 63)));
         System.out.println("Print area of circle: " + ans.AreaOfCircle(5));
